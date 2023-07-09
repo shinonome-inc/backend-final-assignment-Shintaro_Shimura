7fffedbbb36305b0b79b633bf4b512609dab11cf
Shintaro/Shintaro Shimura <106141068+nta808015@users.noreply.github.com>
2023-07-09 14:17:23 +0900

    Revert "ログインログアウト機能実装"

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,15 @@
-from django.conf import settings
-from django.contrib.auth import authenticate, get_user_model, login
-from django.contrib.auth import views as auth_views
-from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView
 
 from .forms import SignupForm
-
-User = get_user_model()
 
 
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
     success_url = reverse_lazy("tweets:home")
-    success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -23,18 +18,3 @@ class SignupView(CreateView):
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
-
-
-class LoginView(auth_views.LoginView):
-    template_name = "accounts/login.html"
-
-
-class Lououtview(auth_views.LogoutView):
-    pass
-
-
-class UserProfileView(LoginRequiredMixin, DetailView):
-    template_name = "accouts/profile.html"
-    model = User
-    slug_url_kwarg = "username"
-    slug_field = "username"
--- mysite/settings.py
+++ mysite/settings.py
@@ -126,11 +126,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-AUTH_USER_MODEL = "accounts.User"
-
-LOGIN_URL = "account:login"
-
-LOGIN_REDIRECT_URL = "tweets:home"
-
-LOGOUT_REDIRECT_URL = "accounts:login"
