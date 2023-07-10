73318631bfa8a93f188f786b32294a69d464e08b
Shintaro Shimura <simura808015@gmail.com>
2023-07-10 13:16:04 +0900

    ログインログアウト機能1次レビュー修正2

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib.auth import authenticate, get_user_model, login
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.mixins import LoginRequiredMixin
